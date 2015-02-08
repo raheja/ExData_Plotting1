050251a693d25d49e9c28a10b86d2cf98ba0c8f0
Urvashi Bhattacharyya <urvashi.raheja@gmail.com>
2015-02-08 22:42:35 +0530

    Assignment commit 2

--- plot4.R
+++ plot4.R
@@ -20,7 +20,7 @@ with(filer, {
   plot(DateTime, Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
   lines(filer$DateTime, filer$Sub_metering_2, col = "red")
   lines(filer$DateTime, filer$Sub_metering_3, col = "blue") 
-  legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1,1,1), col = c( "black", "red", "blue"))
+  legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n", lty = c(1,1,1), col = c( "black", "red", "blue"))
   plot(DateTime, Global_reactive_power, xlab = "datetime", type = "l")
 })
 
